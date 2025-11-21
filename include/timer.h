23924859ef786500808391a25bac6538ed382d2e
MGT-21 <mgt21inv@gmail.com>
2025-11-21 17:53:22 -0300

    :boom: fix: ajustando pequeno erro no timer.h referente ao include guard

--- include/timer.h
+++ include/timer.h
@@ -4,6 +4,8 @@
  * Author: Tiago Barros
  * Based on "From C to C++ course - 2002"
 */
+#ifndef __TIMER_H__
+#define __TIMER_H__
 
 void timerInit(int valueMilliSec);
 
@@ -14,3 +16,5 @@ void timerUpdateTimer(int valueMilliSec);
 int timerTimeOver();
 
 void timerPrint();
+
+#endif
