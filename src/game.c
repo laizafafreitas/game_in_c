c90e899be7d24f5b1199116ff73af7c1b7f13f9a
MGT-21 <mgt21inv@gmail.com>
2025-11-24 21:32:06 -0300

    :recycle: refactor: Ajuste de escalabilidade do game.c

--- src/game.c
+++ src/game.c
@@ -7,47 +7,51 @@
 #include "render.h"
 #include "utils.h"
 
+#define ROUND_TIME 70;
+
+void initGame(GameState *game) {
+    // aqui você só inicializa os lutadores
+    initFighter(&game->player, SCRSTARTX + 5,  1);
+    initFighter(&game->cpu,    SCRENDX   - 5, -1);
+    game-> timeLeft = ROUND_TIME;
+}
+
 void runFight(void) {
-    Fighter player;
-    Fighter cpu;
+    GameState game;
+    initGame(&game);
 
-    initFighter(&player, SCRSTARTX + 5,  1);
-    initFighter(&cpu,    SCRENDX   - 5, -1);
-
-    int running = 1;
-
-    int timeLeft = 70;
+    int running  = 1;
     time_t startTime = time(NULL);
 
     while (running) {
         // INPUT
-        handlePlayerInput(&running, &player);
+        handlePlayerInput(&running, &game.player);
 
         // limita posição do player dentro da arena
-        player.x = clamp(player.x, SCRSTARTX + 1, SCRENDX - 1);
+        game.player.x = clamp(game.player.x, SCRSTARTX + 1, SCRENDX - 1);
 
         // UPDATE (somente em tick de timer)
         if (timerTimeOver()) {
             
             time_t now = time(NULL);
             int elapsed = (int)(now - startTime);
-            timeLeft = 70 - elapsed;
+            game.timeLeft = 70 - elapsed;
             
-            updateCPU(&cpu, &player);
+            updateCPU(&game.cpu, &game.player);
 
             // ataques (aplicam dano e atualizam timers)
-            updateAttack(&player, &cpu);
-            updateAttack(&cpu,    &player);
+            updateAttack(&game.player, &game.cpu);
+            updateAttack(&game.cpu,    &game.player);
 
             // verifica fim de jogo
-            if (player.hp <= 0 || cpu.hp <= 0 || timeLeft <= 0) {
+            if (game.player.hp <= 0 || game.cpu.hp <= 0 || game.timeLeft <= 0) {
                 running = 0;
             }
 
             // DRAW
-            drawGame(&player, &cpu, timeLeft);
+            drawGame(&game.player, &game.cpu, game.timeLeft);
         }
     }
 
-    drawEndScreen(&player, &cpu);
+    drawEndScreen(&game.player, &game.cpu);
 }
